91b9d26a70c611f7da68a951ce1b77916950e2fd
waseeld <dddwas12@gmail.com>
2021-02-02 14:27:25 +0300

    add findIndex && updata && save to Ranidb

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const fs = require('fs');
 const shortid = require('shortid');
 var lodash = require('lodash');
+const { findIndex } = require('lodash');
 
-module.exports = class Ranidb {
+class Ranidb {
     constructor(path_db) {
         this.path_db = path_db;
+    }
+
+    save(data) {
+        data = JSON.stringify(data);
+        fs.writeFileSync(this.path_db, data, { encoding: "utf-8" }, err => {
+            if (err) {
+                return console.error(err)
+            }
+        })
+        //done
+        console.log("done");
     }
 
     getAll() {
@@ -19,14 +31,7 @@ module.exports = class Ranidb {
             ...data
         };
         db.push(data);
-        db = JSON.stringify(db);
-        fs.writeFileSync(this.path_db, db, { encoding: "utf-8" }, err => {
-            if (err) {
-                return console.error(err)
-            }
-
-            //done
-        })
+        this.save(db);
         return data
     }
 
@@ -35,8 +40,22 @@ module.exports = class Ranidb {
         return lodash.find(db, data);
     }
 
-    filter(data){
+    findIndex(data){
+        let db = this.getAll();
+        return lodash.findIndex(db, data);
+    }
+
+    filter(data) {
         let db = this.getAll();
         return lodash.filter(db, data);
     }
+
+    updata(id, data) {
+        let db = this.getAll();
+        let index = this.findIndex({_id: id});
+        db[index] = data;
+        return this.save(db);
+    }
 }
+
+module.exports = Ranidb
