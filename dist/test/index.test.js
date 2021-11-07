7232c872a1e7c7e43b53f9fb26a03a29ab69fbee
waseeld <dddwas12@gmail.com>
2021-11-07 13:32:05 +0300

    frist try to convert to TypeScript

--- dist/test/index.test.js
+++ dist/test/index.test.js
@@ -1,56 +1,44 @@
+"use strict";
 const Ranidb = require("../dist/src/index");
 var expect = require("chai").expect;
 const fs = require("fs");
-
-const db = new Ranidb("./db/data.json")
-
+const db = new Ranidb("./db/data.json");
 db.getAll();
-
 let test = { "_id": "GT6DhK6KXb", "user": "test", "age": 15, "active": false };
-
 describe("Functions", function () {
     it("getAll()", () => {
         let allData = db.getAll();
         let readAllData = fs.readFileSync("./db/data.json", { encoding: "utf-8" });
         readAllData = JSON.parse(readAllData);
-
         expect(allData.length).equal(readAllData.length);
     });
-
     it("push()", () => {
         let allDataPerv = db.getAll();
         db.push(test);
         let allDataNow = db.getAll();
         expect(allDataNow.length).greaterThan(allDataPerv.length);
     });
-
     it("find()", () => {
         let data = db.find(test);
         let allData = db.getAll();
-
         expect(data._id).equal(allData[2]._id);
     });
-
     it("fillter()", () => {
         let data = db.filter(test);
         expect(Object.keys(data[0]).length).equal(Object.keys(test).length);
     });
-
     it("updata()", () => {
         let data = db.find(test);
-        data.ss = "asdsad"
+        data.ss = "asdsad";
         db.updata(test, data);
         let alldata = db.getAll();
         expect(alldata[2].ss).equal("asdsad");
     });
-
     it("clear()", () => {
-        let clear = db.clear()
-        let allDataNow = db.getAll()
-
-        expect(clear == true && allDataNow.length == 0).equal(true)
-    })
-
+        let clear = db.clear();
+        let allDataNow = db.getAll();
+        expect(clear == true && allDataNow.length == 0).equal(true);
+    });
     it("save()", () => {
         let startData = [
             {
@@ -65,26 +53,22 @@ describe("Functions", function () {
                 "age": 40,
                 "active": false
             }
-        ]
-
-        db.save(startData)
-        let allDataNow = db.getAll()
-
-        expect(startData.length).equal(allDataNow.length)
-    })
+        ];
+        db.save(startData);
+        let allDataNow = db.getAll();
+        expect(startData.length).equal(allDataNow.length);
+    });
     // it("fillter().put()", () => {
     //     let dd = db.filter(test).put({ name: "salim", ...test });
     //     let allDataNow = db.getAll();
     //     expect(allDataNow[3].name).equal("salim");
     // });
-
     // it("fillter().updata()", () => {
     //     let data = db.find(test);
     //     db.filter(test).updata({ ...data, ss: "xzc" });
     //     let alldata = db.getAll();
     //     expect(alldata[3].ss).equal("xzc");
     // });
-
     // it("fillter().delete()", () => {
     //     let allDataPerv = db.getAll();
     //     db.filter(test).delete();
@@ -92,3 +76,4 @@ describe("Functions", function () {
     //     expect(allDataNow.length).lessThan(allDataPerv.length);
     // });
 });
+//# sourceMappingURL=index.test.js.map
---
+++ src/index.ts
@@ -0,0 +1,249 @@
+import * as fs from 'fs-extra';
+
+import * as shortid from 'shortid';
+
+import * as lodash from 'lodash'
+import { extend } from 'lodash';
+
+type ty = "random" | "empty" | "gradual";
+
+interface Options {
+    idType: ty
+}
+
+// class raniObj extends Object {
+//     filter() {
+//       console.log(this.filter.that);
+//     }
+  
+//     delete() {
+//       return this.filter.that.delete();
+//     }
+  
+//     updata(data) {
+//       return this.filter.that.updata([data]);
+//     }
+  
+//     put(data) {
+//       return this.filter.that.put(data);
+//     }
+// }
+
+// class raniArray extends Array {
+//     db() {
+//       return this.db.that;
+//     }
+  
+//     delete() {
+//       let db = this.db();
+//       this.forEach((e) => {
+//         let data = db.getAll();
+  
+//         data.splice(db.findIndex(e), 1);
+  
+//         db.save(data);
+//       });
+//       return db.getAll();
+//     }
+  
+//     updata(data = []) {
+//       let db = this.db();
+//       let array = db.getAll();
+  
+//       this.forEach((e) => {
+//         let newItem;
+//         if (!Array.isArray(data)) {
+//           newItem = data;
+//         } else {
+//           newItem = data[this.indexOf(e)];
+//         }
+//         let index = db.findIndex(e);
+//         array[index] = newItem;
+//       });
+//       db.save(array);
+//       return db.getAll();
+//     }
+  
+//     put(data) {
+//       let db = this.db();
+  
+//       let array = db.getAll();
+  
+//       this.forEach((e) => {
+//         let index = db.findIndex(e);
+//         let newItem;
+//         if (!Array.isArray(data)) {
+//           newItem = data;
+//         } else {
+//           newItem = data[this.indexOf(e)];
+//         }
+//         for (let key in newItem) {
+//           e[key] = newItem[key];
+//         }
+//         array[index] = e;
+//       });
+//       db.save(array);
+  
+//       return db.getAll();
+//     }
+// }
+
+class Ranidb {
+    idType: any;
+    path_db: string;
+    constructor(path_db: string, Options: Options = { idType: "random" }) {
+        this.path_db = path_db;
+        this.setType(Options.idType);
+        this.fileExist();
+    }
+
+    setType(type: ty) {
+        const types: string[] = ["random", "empty", "gradual"];
+        const index: any = types.indexOf(type);
+        if (index !== -1) {
+            this.idType = index + 1;
+        } else {
+            console.log("idType is not correct");
+            this.idType = 1;
+        }
+    }
+
+    fileExist(path = this.path_db) {
+        try {
+            if (fs.existsSync(path)) {
+                // console.log("The db exists.");
+                return true;
+            } else {
+                // console.log('The db does not exist.');
+                // console.log("Don't worry i will create empty db");
+                try {
+                    fs.writeFileSync(path, "[]");
+                    return true;
+                } catch (error) {
+                    let folder = path.split("/");
+                    folder.pop();
+                    fs.mkdirSync(folder.join("/"));
+                    fs.writeFileSync(path, "[]");
+                    return true;
+                }
+            }
+        } catch (err) {
+            console.error(err);
+            return false;
+        }
+    }
+
+    ensureFile(callback: Function) {
+        if (fs.existsSync(this.path_db)) {
+            return callback();
+        } else {
+            this.fileExist();
+            if (fs.existsSync(this.path_db)) {
+                return callback();
+            } else {
+                return "Not found db\nCreate new DB";
+            }
+        }
+    }
+
+    save(data: any) {
+        data = JSON.stringify(data);
+        this.ensureFile(() => {
+            try {
+                fs.writeFileSync(this.path_db, data, { encoding: "utf-8" });
+                return true;
+            } catch (error) {
+                console.error(error);
+                return false;
+            }
+        });
+    }
+
+    getAll(): Array<object> {
+        return this.ensureFile(() => {
+            let data = fs.readFileSync(this.path_db, { encoding: "utf-8" });
+            return JSON.parse(data);
+        });
+    }
+
+    push(data: object): object {
+        let all: any = this.getAll();
+
+        let lastId = all.length ? all[all.length - 1]._id + 1 : 1;
+
+        let db: Array<object> = all;
+
+        let _id: any = undefined;
+
+        if (this.idType === 1) {
+            _id = shortid.generate();
+        } else if (this.idType === 3) _id = lastId;
+
+        data = {
+            _id: _id,
+            ...data,
+        };
+
+        db.push(data);
+        this.save(db);
+        return data;
+    }
+
+    find(data: object){    
+        let db = this.getAll();
+    
+        let result = lodash.find(db, data);
+    
+        if (!result) return {};
+    
+        // let newResult = new raniObj();
+    
+        // for (let item in result) {
+        //   newResult[item] = result[item];
+        // }
+        // newResult.filter.that = this.filter(result);
+    
+        // newResult.filter.that.db.that = this;
+    
+        return result;
+    }
+
+    findIndex(data: object): Number {
+        let db = this.getAll();
+        return lodash.findIndex(db, data);
+    }
+
+    filter(data: object) {
+        let db = this.getAll();
+        
+        // let result = new raniArray();
+    
+        // result = raniArray.from(lodash.filter(db, data));
+    
+        // result.db.that = this;
+
+        let result = lodash.filter(db, data);
+
+        return result;
+    }
+
+    updata(find: object, data: object) {
+        let db = this.getAll();
+        let index: any = this.findIndex(find);
+        db[index] = data;
+        this.save(db);
+        return data;
+    }
+
+    clear(): boolean {
+        try {
+          this.save([]);
+          return true;
+        } catch (error) {
+          console.log(error);
+          return false
+        }
+    }
+}
+
+module.exports = Ranidb;
