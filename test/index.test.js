32d6634c404b0e0e34af726ca3e37422fab4d7d6
salemkode <salem.m.s2030@gmail.com>
2022-03-16 07:27:08 +0300

    Update Test

--- test/index.test.js
+++ test/index.test.js
@@ -1,117 +1,142 @@
-const Ranidb = require("../dist/src/index");
+const Ranidb = require("../dist/index");
 var expect = require("chai").expect;
 const fs = require("fs");
 
-const db = new Ranidb("./db/data.json")
+const db = new Ranidb("./db/data.json");
 
 db.getAll();
 
-let test = { "_id": "GT6DhK6KXb", "user": "test", "age": 15, "active": false };
+let test = { _id: "GT6DhK6KXb", user: "test", age: 15, active: false };
 
 describe("Functions", function () {
-    it("getAll()", () => {
-        let allData = db.getAll();
-        let readAllData = fs.readFileSync("./db/data.json", { encoding: "utf-8" });
-        readAllData = JSON.parse(readAllData);
+  it("getAll()", () => {
+    let allData = db.getAll();
+    let readAllData = fs.readFileSync("./db/data.json", { encoding: "utf-8" });
+    readAllData = JSON.parse(readAllData);
 
-        expect(allData.length).equal(readAllData.length);
-    });
+    expect(allData.length).equal(readAllData.length);
+  });
 
-    it("push()", () => {
-        let allDataPerv = db.getAll();
-        db.push(test);
-        let allDataNow = db.getAll();
-        expect(allDataNow.length).greaterThan(allDataPerv.length);
-    });
+  it("push()", () => {
+    let allDataPerv = db.getAll();
+    db.push(test);
+    let allDataNow = db.getAll();
+    expect(allDataNow.length).greaterThan(allDataPerv.length);
+  });
 
-    it("find()", () => {
-        let data = db.find(test);
-        let allData = db.getAll();
+  it("find()", () => {
+    let startData = [
+      {
+        _id: "OHILvgy0ZD",
+        user: "fred",
+        age: 40,
+        active: false,
+      },
+      {
+        _id: "GT6DhK6KXb",
+        user: "test",
+        age: 15,
+        active: false,
+      },
+    ];
+    db.save(startData);
+    let data = db.find(startData[1]);
 
-        expect(data._id).equal(allData[2]._id);
-    });
+    expect(data._id).equal(startData[1]._id);
+  });
 
-    it("fillter()", () => {
-        let data = db.filter(test);
-        expect(Object.keys(data[0]).length).equal(Object.keys(test).length);
-    });
+  it("fillter()", () => {
+    let startData = [
+      {
+        _id: "OHILvgy0ZD",
+        user: "fred",
+        age: 40,
+        active: false,
+      },
+      {
+        _id: "GT6DhK6KXb",
+        user: "test",
+        age: 15,
+        active: false,
+      },
+    ];
+    db.save(startData);
+    let data = db.filter(test);
+    expect(Object.keys(data[0]).length).equal(Object.keys(test).length);
+  });
 
-    it("updata()", () => {
-        let data = db.find(test);
-        data.ss = "asdsad"
-        db.updata(test, data);
-        let alldata = db.getAll();
-        expect(alldata[2].ss).equal("asdsad");
-    });
+  it("updata()", () => {
+    let startData = [
+      {
+        _id: "OHILvgy0ZD",
+        user: "fred",
+        age: 40,
+        active: false,
+      },
+      {
+        _id: "GT6DhK6KXb",
+        user: "test",
+        age: 15,
+        active: false,
+      },
+    ];
+    db.save(startData);
+    let data = db.find(test);
+    data.ss = "asdsad";
+    db.updata(test, data);
+    let alldata = db.getAll();
+    expect(alldata[1].ss).equal("asdsad");
+  });
 
-    it("clear()", () => {
-        let clear = db.clear()
-        let allDataNow = db.getAll()
+  it("clear()", () => {
+    let clear = db.clear();
+    let allDataNow = db.getAll();
 
-        expect(clear == true && allDataNow.length == 0).equal(true)
-    })
+    expect(clear == true && allDataNow.length == 0).equal(true);
+  });
 
-    it("delete()", () => {
-        let startData = [
-            {
-                "_id": "JfMOsuESH",
-                "user": "barney",
-                "age": 36,
-                "active": true
-            },
-            {
-                "_id": "OHILvgy0ZD",
-                "user": "fred",
-                "age": 40,
-                "active": false
-            }
-        ]
+  it("delete()", () => {
+    let startData = [
+      {
+        _id: "JfMOsuESH",
+        user: "barney",
+        age: 36,
+        active: true,
+      },
+      {
+        _id: "OHILvgy0ZD",
+        user: "fred",
+        age: 40,
+        active: false,
+      },
+    ];
 
-        db.save(startData)
-        let clear = db.delete({active: false})
-        let allDataNow = db.getAll()
-        // console.log(allDataNow, clear);
-        expect(allDataNow.length < startData.length).equal(true)
-    })
+    db.save(startData);
+    let clear = db.delete({ active: false });
+    let allDataNow = db.getAll();
+    // console.log(allDataNow, clear);
+    expect(allDataNow.length < startData.length).equal(true);
+  });
 
-    it("save()", () => {
-        let startData = [
-            {
-                "_id": "JfMOsuESH",
-                "user": "barney",
-                "age": 36,
-                "active": true
-            },
-            {
-                "_id": "OHILvgy0ZD",
-                "user": "fred",
-                "age": 40,
-                "active": false
-            }
-        ]
+  it("save()", () => {
+    let startData = [
+      {
+        _id: "JfMOsuESH",
+        user: "barney",
+        age: 36,
+        active: true,
+      },
+      {
+        _id: "OHILvgy0ZD",
+        user: "fred",
+        age: 40,
+        active: false,
+      },
+    ];
 
-        db.save(startData)
-        let allDataNow = db.getAll()
+    db.save(startData);
+    let allDataNow = db.getAll();
 
-        expect(startData.length).equal(allDataNow.length)
-    })
-    // it("fillter().put()", () => {
-    //     let dd = db.filter(test).put({ name: "salim", ...test });
-    //     let allDataNow = db.getAll();
-    //     expect(allDataNow[3].name).equal("salim");
-    // });
-
-    // it("fillter().updata()", () => {
-    //     let data = db.find(test);
-    //     db.filter(test).updata({ ...data, ss: "xzc" });
-    //     let alldata = db.getAll();
-    //     expect(alldata[3].ss).equal("xzc");
-    // });
-
-    // it("fillter().delete()", () => {
-    //     let allDataPerv = db.getAll();
-    //     db.filter(test).delete();
-    //     let allDataNow = db.getAll();
-    //     expect(allDataNow.length).lessThan(allDataPerv.length);
-    // });
+    expect(startData.length).equal(allDataNow.length);
+  });
 });
